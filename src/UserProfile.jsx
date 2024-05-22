50f375ad31b905ae2471885823ff9d6f0c47fcf4
Odinakachukwu <odijoe029@gmail.com>
2024-05-22 11:25:50 -0700

    Added initial commits for UserProfile.jsx for the friends screen

---
+++ src/UserProfile.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const UserProfile = () => {
+  return (
+    <div className="purple_container">
+      <img src="assets/image 14.png" alt="Avatar" className="avatar_photo" />
+      <img src="assets/settings-32.png" alt="Settings" className="setting_icon" />
+      <img src="assets/search-32.png" alt="Search" className="search_icon" />
+      <h2>Chukwuka</h2>
+      <h4 className="text_cover">PIN: 0123Jux7</h4>
+    </div>
+  );
+};
+
+export default UserProfile;
